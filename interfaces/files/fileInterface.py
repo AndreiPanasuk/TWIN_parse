2727210752bcb8bc2d9d99e48c0e13cc76a485ae
panasyuk <apn256@gmail.com>
2021-07-26 22:07:20 +0300

    add fileInterface'

---
+++ interfaces/files/fileInterface.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+''' FileInterface
+    
+    Интефейс доступа к файлам
+    
+    Параметры инициализации:
+    args
+        name    str    - название интерфейса
+    kargs
+        base_path str    - базовый каталог    
+        encoding    str    - кодировка
+        
+'''
+import os
+
+class FileInterface(object):
+    
+    def __init__(self, name, base_path = None, encoding = 'UTF8'):
+        super().__init__(name)
+        self._base_path = os.path.normpath(base_path)
+        self._encoding = encoding
+    
+    ''' get
+    
+        Чтение файла
+        Параметры:
+            fname    str    - относительный путь к файлу
+        Возврат:
+            str    - содержимое файла
+        
+    '''   
+    def get(self, fname):
+        fpath = self.full_path(fname)
+        with open(fpath, 'r', encoding = self._encoding) as fp:
+            return fp.read()
+
+    ''' send
+    
+        Запись файла
+        Параметры:
+            fname    str    - относительный путь к файлу
+            data    str    - данные (содержимое файла)
+    '''   
+    def send(self, fname, data):
+        fpath = self.full_path(fname)
+        with open(fpath, 'w') as fp:
+            fp.write(data)
+    
+    ''' full_path
+    
+        Сформировать путь к файлу с учетом базового каталога
+        Параметры:
+            fname    str    - относительный путь к файлу
+        Возврат:
+            str    - путь к файлу с учетом базового каталога
+        
+    '''   
+    def full_path(self, fname):
+        fname = os.path.normpath(fname)
+        if self._base_path:
+            return os.path.join(self._base_path, fname)
+        else:
+            return fname
---
+++ interfaces/files/jsonInterfaces.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+''' JSONInterface
+    
+    Интефейс доступа к JSON файлам
+    
+    Параметры инициализации:
+    args
+        name    str    - название интерфейса
+    kargs
+        ident    int    - отступ при записи
+        base_path str    - базовый каталог
+        encoding    str    - кодировка
+'''
+import json
+import codecs
+
+class JSONInterface(object):
+
+    def __init__(self, *args, ident = 4, **kargs):
+        super().__init__(*args, **kargs)
+        self._ident = ident
+    
+    ''' get
+    
+        Чтение JSON файла
+        Параметры:
+            fname    str    - относительный путь к файлу
+        Возврат:
+            dict/list    - содержимое файла преобразованное
+        
+    '''   
+    def get(self, fname):
+        fpath = self.full_path(fname)
+        with open(fpath, 'r', encoding = self._encoding) as fp:
+            return json.load(fp)
+
+    ''' send
+    
+        Запись файла
+        Параметры:
+            fname    str    - относительный путь к файлу
+            data    str    - данные (содержимое файла)
+    '''   
+    def send(self, fname, data):
+        fpath = self.full_path(fname)
+        with open(fpath, 'wb') as fp:
+            json.dump(data, codecs.getwriter(self._encoding)(fp), ensure_ascii = False, indent = self._ident)
+    
